2ba3435f37877a55a9df23372a8c59fd856d0489
AL-garib <algarib.a.g@gmail.com>
2018-06-17 02:44:46 -0400

    add datebise

---
+++ database/migrations/2018_06_17_092044_create_personal_datas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonalDatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal_datas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('identity');
+            $table->date('date_brith');
+            $table->string('place_brith');
+            $table->date('expiry_date_id');
+            $table->string('nationality');
+            $table->string('city');
+            $table->string('image_person');
+            $table->string('image_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personal_datas');
+    }
+}
